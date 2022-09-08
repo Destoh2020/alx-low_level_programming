19efbaf000eac7790754ac3a2e2f17087f69cece
Destoh2020 <desterianragira@gmail.com>
2022-09-08 05:52:17 -0700

    Printing size of data types in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,9 @@
  */
 int main(void)
 {
-	printf("Size of int: %zu bytes\n", sizeof(int));
-	printf("Size of float: %zu bytes\n", sizeof(float));
-	printf("Size of double: %zu bytes\n", sizeof(double));
-	printf("Size of char: %zu bytes\n", sizeof(char));
-	printf("Size of long int: %zu bytes\n" sizeof(long int));
-	printf("Size of long long int: %zu bytes\n", sizeof(long long int));
+	printf("Size of int: %lu bytes\n", sizeof(int));
+	printf("Size of float: %lu bytes\n", sizeof(float));
+	printf("Size of double: %lu bytes\n", sizeof(double));
+	printf("Size of char: %lu bytes\n", sizeof(char));
 	return (0);
 }
