90fe2ab0f718d2c3190faa4cfa0bb080405144cb
Destoh2020 <desterianragira@gmail.com>
2022-09-14 01:25:08 -0700

    Printing 10 times lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,14 @@ void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
+	
+	i = 0;
 
-	while (i = 0; i < 10)
+	while (i < 10)
 	{
-		while (ch = 'a'; ch <= 'z')
+	
+		ch = 'a';
+		while (ch <= 'z')
 		{
 			_putchar(ch);
 			ch++;
