352c6077357e170e1f4b488970e99e24e33a6b49
Destoh2020 <desterianragira@gmail.com>
2022-09-14 02:27:31 -0700

    Printing sign of numbers as + or -

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ *
+ * @n: number to be checked
+ *
+ * Return: 1 and print + if number is greater than zero
+ * 0 and print 0 if number is 0
+ * -1 and print - if number is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar("+");
+		return (1);
+	}
+	else if (n == 0);
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar("-");
+	}
+	return (-1);
+}
