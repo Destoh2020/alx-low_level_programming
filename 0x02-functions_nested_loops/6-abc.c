e9cd3ca2ebee31c8f47a8af34664a67741dbf5fa
Destoh2020 <desterianragira@gmail.com>
2022-09-14 02:57:45 -0700

    Computing absolute value of anteger

---
+++ 0x02-functions_nested_loops/6-abc.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - Computes absolute values of number
+ *
+ * @n the number to check
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n)
+	}
+}
