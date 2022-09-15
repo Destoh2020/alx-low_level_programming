ecdca6546923b58a360a39c56b6835a019b8917e
Destoh2020 <desterianragira@gmail.com>
2022-09-14 23:47:30 -0700

    Debugging largest number

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - cause an infite loop
+ *
+ * @\\o/\n: what to be printed followed by a new line
  *
  * Return: Alway 0 (Success)
  */
@@ -12,10 +14,11 @@ int main(void)
 
 	i = 0;
 
-/*	while (i < 10)
-	{
-		putchar(i);
-	}
+/**
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
 */
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
