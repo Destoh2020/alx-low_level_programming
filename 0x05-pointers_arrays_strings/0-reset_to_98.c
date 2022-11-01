7f80e8d7a1cf6a7ae1aa84fcc6ebd217c35bf659
Destoh2020 <desterianragira@gmail.com>
2022-10-31 23:08:24 -0700

    Pointers and arrays here

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to _98 - update the value it points to 98
+ *
+ * @n: pointer to integer
+ *
+ * Return: no return
+ */
+void reset_to_98(int *n)
+{
+	*n =98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+In this directory we are going to work with pointers and arrays
